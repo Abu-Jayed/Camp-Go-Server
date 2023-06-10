6ef8c83f21112c37e877f0cfb39300b41f523c99
Abu-Jayed <aj.webdevlopment@gmail.com>
2023-06-10 10:07:09 +0600

    class data sorted successfully

--- index.js
+++ index.js
@@ -15,7 +15,6 @@ app.use(express.json());
 // code from data base start
 
 const { MongoClient, ServerApiVersion } = require('mongodb');
-// const uri = "mongodb+srv://<username>:<password>@cluster0.pwifs1n.mongodb.net/?retryWrites=true&w=majority";
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.pwifs1n.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -31,12 +30,24 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+    const classCollection = client.db("campGo").collection("classes")
+
+    // classes related api
+    app.get('/classes', async (req, res) => {
+      const result = await classCollection.find().sort({enrolled:-1}).toArray();
+      res.send(result);
+    });
+
+
+
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
